ed4a7f5145078a9e59194e2fe6b1a716bf72ac1f
David Borge <david.borge.olmedo@gmail.com>
2017-10-12 00:00:04 +0200

    Versión 2.0
    Solitando trabajar para ONGs.

---
+++ index.php
@@ -0,0 +1,82 @@
+<?php
+  include 'header.html';
+?>
+
+<main>
+	<div class="container">
+		<div class="section">
+					
+			<!-- MAIN CONTENT -->
+	
+			<amp-fit-text width="300" height="50" layout="responsive" max-font-size="52" class="title-of-page text-center-h">
+				If you think you can help people and you need a website, do let me know.
+			</amp-fit-text>
+			
+			<p class="flow-text text-justify">I'm a web developer. And, honestly, I want my work to be fulfilling, otherwise I don't see the point in doing it. That's why I like working for charities.</p>
+			<p class="flow-text text-justify">At the moment, I'm working for <a href="http://www.greaterchernobylcause.ie/" target="_blank">The Greater Chernobyl Cause</a> (still under construction).</p>
+			
+			<!--Tarjeta 1: Work Experience-->
+			<div class="card">
+				<amp-fit-text width="300" height="25" layout="responsive" max-font-size="52" class="text-center-h text-orange">
+					Work Experience
+				</amp-fit-text>
+				<hr>
+
+				<div class="space-around">
+					<div class="flex-item">
+						<amp-fit-text width="300" height="50" layout="responsive" max-font-size="52" class="text-center-h">
+							Unique Web Developer at<br>
+							<b>The Greater Chernobyl Cause (Ireland)</b><br>
+							(Under construction)
+						</amp-fit-text>
+						<a href="http://www.greaterchernobylcause.ie/" target="_blank">
+							<amp-img id="logo" src="./img/tgcc.jpg" width="625" height="260" layout="responsive" alt="The Greater Chernobyl Cause"></amp-img>
+						</a>
+					</div>
+					<div class="flex-item">
+						<amp-fit-text width="300" height="50" layout="responsive" max-font-size="52" class="text-center-h">
+							Unique Web Developer at<br>
+							<b>Montajes JuanFran S.L. (Spain)</b><br>
+							(Under construction)
+						</amp-fit-text>
+						<a href="http://montajesjuanfran.com/" target="_blank">
+							<amp-img id="logo" src="./img/montajes-juanfran.png" width="276" height="263" layout="responsive" alt="The Greater Chernobyl Cause"></amp-img>
+						</a>
+					</div>
+				</div>
+			</div>
+
+			<!--Tarjeta 2: Languages-->
+			<div class="card">
+				<amp-fit-text width="300" height="25" layout="responsive" max-font-size="52" class="text-center-h text-orange">
+					Languages
+				</amp-fit-text>
+				<hr>
+
+				<div class="space-around">
+					<div class="flex-item">
+						<amp-fit-text width="300" height="50" layout="responsive" max-font-size="52" class="text-center-h">
+							<b>English</b><br>
+							C1 Level
+						</amp-fit-text>
+					</div>
+					<div class="flex-item">
+						<amp-fit-text width="300" height="50" layout="responsive" max-font-size="52" class="text-center-h">
+							<b>Spanish</b><br>
+							Mother tongue
+						</amp-fit-text>
+					</div>
+				</div>
+			</div>
+
+
+			<!-- CV -->
+			<a href="https://drive.google.com/open?id=0BxI70cRVixbYXzg4SzdSODY5MEE" class="waves-effect waves-light btn-large orange darken-3 max-width flow-text cv-content"><i class="material-icons left">picture_as_pdf</i>Download Curriculum Vitae<i class="material-icons right">picture_as_pdf</i></a>
+
+		</div>
+	</div>
+</main>
+
+<?php
+  include 'footer.html';
+?>
